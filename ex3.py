522fe1146be987f65196660248592232c0cddb35
ZhiguangWang <zhiguangwang2017@yahoo.com>
2017-04-12 15:37:36 +0800

    Create ex3.py
    数数

---
+++ ex3.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+#打印开始数鸡
+print 'I will now count my chickens:'
+
+#打印hens数量
+print 'Hens',25 + 30/6
+#!!!这里要注意，%号是求余数的，75/4余3，这样100减下来是97.
+print 'Roosters' ,100-25*3%4
+
+#现在数蛋
+print 'Now I will count the eggs:'
+
+#！！！这里的1/4是由于没有整数，因此就直接省略了。
+print 3+2+1-5+4%2-1/4+6
+
+#打印
+print 'Is it true that 3+2<5-7?'
+
+#判断
+print 3+2<5-7
+
+#打印+求值
+print "What is 3+2?",3+2
+print "What is 5-7?",5-7
+
+
+print "Oh,that's why it's False."
+
+print 'How about some more?'
+print 'Is it greater?',5>-2
+print 'Is it greater or equal?',5>=2
+print 'Is it less or equal?',5<=2
+
+
+print 3.+2.+1.-5.+4.%2.-1./4.+6.
