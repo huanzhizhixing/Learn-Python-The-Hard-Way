65ad2055672ca43df0bb98f0dd877080308f4742
ZhiguangWang <zhiguangwang2017@yahoo.com>
2017-04-12 17:20:17 +0800

    Update ex25-1.py

--- ex25-1.py
+++ ex25-1.py
@@ -37,3 +37,61 @@ def print_first_and_last_sorted(sentence):
 	print_last_word(words)
 	
 
+
+"""
+>>> import ex25
+>>> sentence = "All good things come to those who wait."
+>>> words=ex25.break_words(sentence)
+>>> words
+['All', 'good', 'things', 'come', 'to', 'those', 'who', 'wait.']
+>>> sorted_words
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'sorted_words' is not defined
+>>> sort_words
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'sort_words' is not defined
+>>> sort_words = ex25.sort_words(words)
+>>> sorted_words
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'sorted_words' is not defined
+>>> sorted_words = ex25.sort_words(words)
+>>> sorted_words
+['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+>>> ex25.print_first_word(words)
+All
+>>> ex25.print_last_word(words)
+wait.
+>>> words
+['good', 'things', 'come', 'to', 'those', 'who']
+>>> wrods
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'wrods' is not defined
+>>> ex25.print_first_word(sorted_words)
+All
+>>> ex25.print_last_word(sorted_word)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'sorted_word' is not defined
+>>> ex25.print_last_word(sorted_words)
+who
+>>> sorted_words
+['come', 'good', 'things', 'those', 'to', 'wait.']
+>>> sorted_words = ex25.sort_sentence(sentence)
+>>> sorted_words
+['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+>>> ex25.print_first_and_last(sentence)
+All
+wait.
+>>> ex25.print_first_and_last_sorted(sentence)
+All
+who
+>>>
+"""
+
+
+
+
