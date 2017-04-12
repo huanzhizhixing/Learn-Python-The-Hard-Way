3a1ae45dfc0d2f399b45ec89502a2ca713ffbf51
ZhiguangWang <zhiguangwang2017@yahoo.com>
2017-04-12 16:41:28 +0800

    *arg,接收多个变量

---
+++ ex18.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+#This one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2 =args
+	#注意，要严格对齐，空格是多少，就是多少。
+    print "arg1: %r, arg2: %r " % (arg1,arg2)
+	
+# Okey, that *args is actually pointless, we can just do this
+def print_two_again(arg1,arg2):
+    print "arg1: %r ,arg2: %r " % (arg1, arg2)
+	
+#this just take one argument
+def print_one(arg1):
+    print "arg1: %r " % arg1
+	
+#this one takes no arguments
+#函数后面的”：”号不可少。
+def print_none():
+    print "I got nothin'."
+	
+	
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
