8dcca6adfe1daaf7aca3f8c1a097f2b158e4e23e
zhiguangwang <zhiguangwang2017@yahoo.com>
2017-04-12 15:16:38 +0800

    打印
    初学打印

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#print 'hello world!'
+#print 'hello agaim'
+#print 'I like typing this.'
+#print 'This is fun.'
+#print "Yay! printing."
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+print 'only one'
+
