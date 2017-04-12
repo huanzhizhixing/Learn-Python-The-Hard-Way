0240e44665be25c6355c8cc440e35ee434b31027
ZhiguangWang <zhiguangwang2017@yahoo.com>
2017-04-12 15:57:23 +0800

    argv

---
+++ ex13.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is :", first
+print "Your secong variable is :", second
+print "your third variable is :", third
+ 
+
+place = raw_input("Where are you?")
+
+print "So, you are in %s " % place
