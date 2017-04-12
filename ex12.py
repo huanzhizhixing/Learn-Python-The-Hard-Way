e6ff6972cee1a2aa659a4a15c18876cff54ed9bc
ZhiguangWang <zhiguangwang2017@yahoo.com>
2017-04-12 15:53:09 +0800

    raw_input()

---
+++ ex12.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+
+print "So, you're %s old, %s tall and %r heavy." % (
+    age, height,weight)
+	
+
+ 
+
