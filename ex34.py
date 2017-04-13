20cc43c75e5faa36ded37f9204364fb857e493eb
ZhiguangWang <zhiguangwang2017@yahoo.com>
2017-04-13 10:58:21 +0800

    列表的位置

---
+++ ex34.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+animals = ['bear', 'python', 'peacock','kangaroo','whale','platypus']
+a=animals
+
+a1 = a[1]
+a2 = a[2]
+a3 = a[0]
+a4 = a[3]
+a5 = a[4]
+a6 = a[3]
+a7 = a[5]
+a8 = a[4]
+
+print a1,a2,a3,a4,a5,a6,a7,a8
+
+
